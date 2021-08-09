ae30b320b11b7b6e1b3a10f9606f4bf70e3bda8a
Kishan Baldaniya <kishanbaldaniya2@gmail.com>
2021-08-09 20:01:14 +0530

    doctor-portal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Login from "./component/Login";
+import Signup from "./component/Signup";
+import DashBoard from "./component/DashBoard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/dashboard" component={DashBoard} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
